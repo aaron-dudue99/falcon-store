47648ebefcf3c52878512057406664a79bd35b70
Aaron Dudue <duduzilesibanda99@gmail.com>
2020-08-15 22:30:49 +0200

    created models, done with rendering store and cart

--- store/views.py
+++ store/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 def index(request):
     context={}
-    return render(request,'store/index.html',context = context )
+    return render(request,'store/index.html', context)
 
 def store(request):
-    context={}
+    products = Product.objects.all()
+    context={'products':products}
     return render(request,'store/store.html',context = context )
 
 def cart(request):
-    context={}
-    return render(request,'store/cart.html',context = context )
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer = customer, complete = False)
+        items = order.orderitem_set.all()
+
+    else:
+        items = []
+
+
+    context={'items':items}
+    return render(request,'store/cart.html',context )
 
 def checkout(request):
     context={}
